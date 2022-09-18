70dda35e89c19c640b7d4f31ad7974aed007adaa
zhangsong <zhangsong@qq.com>
2022-09-18 15:16:44 +0800

    hot-fix first

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello gi2");
         System.out.println("hello git4");
     }
 }
